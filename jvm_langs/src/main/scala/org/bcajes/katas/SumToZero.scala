6e159e6a709dc7ede384284953e23578ac31d13c
briancajes <brian.cajes@gmail.com>
2013-12-22 16:03:52 -0500

    additional tests

--- jvm_langs/src/main/scala/org/bcajes/katas/SumToZero.scala
+++ jvm_langs/src/main/scala/org/bcajes/katas/SumToZero.scala
@@ -1,11 +1,9 @@
 class SumToZero{
-    def process(arr: Array[Int]): Iterator[Array[Int]] = {
-	for (combination <- arr.combinations(3); 
-	     if combination.sum == 0) yield
-		   combination
-    } 
-
-
+  def process(arr: Array[Int], size: Int = 3): Iterator[Array[Int]] = {
+    for (combination <- arr.combinations(size)
+         if combination.sum == 0) yield
+      combination
+  }
 }
 
 
--- jvm_langs/test/scala/org/bcajes/katas/test_sum_to_zero.scala
+++ jvm_langs/test/scala/org/bcajes/katas/test_sum_to_zero.scala
@@ -1,17 +1,32 @@
 import org.junit.runner.RunWith
 import org.scalatest.junit._
-import org.scalatest._;
+import org.scalatest._
 import scala.runtime.ScalaRunTime.stringOf
 
 @RunWith(classOf[JUnitRunner])
-class SetSuite extends FunSuite {
+class TestSumToZero extends FunSuite {
 
-  test("should return three nums that sum to zero") {
-  	val stz = new SumToZero; 
-	val it_res = stz.process(Array(1,2,-3,10))
-	    val res = stringOf(it_res.toArray)
-	    assert( res == "Array(Array(1, 2, -3))", 
-		   "res: " + res)
+  test("should return entire array that sums to zero") {
+  	val stz = new SumToZero
+	  val it_res = stz.process(Array(1,2,-3))
+	  val res = stringOf(it_res.toArray)
+	    assert( res == "Array(Array(1, 2, -3))",
+		    "res: " + res)
   }
+
+  test("should return empty iterator for array"){
+    val stz = new SumToZero
+    val it_res = stz.process(Array(1,2,3,4))
+    assert(it_res.hasNext, stringOf(it_res.toArray))
+  }
+
+  test("should return three nums that sum to zero from set of four"){
+    val stz = new SumToZero
+    val it_res = stz.process(Array(125, 124, -100, -25))
+    val res = stringOf(it_res.toArray)
+    assert( res == "Array(Array(125, -100, -25))",
+      "res: " + res)
+  }
+
 
 }
