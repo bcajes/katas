2fed6b4f7e22b527408d5daef431794af61381fe
briancajes <brian.cajes@gmail.com>
2013-12-28 21:01:44 -0500

    added optmized version of SumToZero.  Playing around with traits and pattern matching.

--- jvm_langs/src/main/scala/org/bcajes/katas/SumToZero.scala
+++ jvm_langs/src/main/scala/org/bcajes/katas/SumToZero.scala
@@ -1,13 +1,67 @@
+import util.control.Breaks
+import util.control.Breaks.break
+
+
+
 class SumToZeroNotFound(msg:String=null, cause:Throwable=null)
   extends java.lang.Exception (msg, cause) {}
 
 
-class SumToZero{
-  def process(arr: Array[Int], size: Int = 3): Array[Int] = {
-    for(combination <- arr.combinations(size))
-      if(combination.sum == 0)  //note: can yield combinations here if we want to find all combinations that sum to zero
-        return combination
-    throw new SumToZeroNotFound()
+
+trait SumToZero{
+
+  @throws(classOf[SumToZeroNotFound])
+  def process(arr: Array[Int], size: Int = 3): Array[Int]
+}
+
+
+object SumToZeroBruteForce extends SumToZero{
+  //brute force process any size combination
+  override def process(arr: Array[Int], size: Int = 3): Array[Int] = {
+    arr.combinations(size).find(_.sum == 0).getOrElse(throw new SumToZeroNotFound)
   }
 }
 
+
+object SumToZeroBinSearch extends SumToZero{
+  //optimized for processing size of three
+
+  //scala does not seem to have a native binary search routine
+  implicit private class Search(val arr: Array[Int]){
+    def binSearch(target: Int) = {
+      java.util.Arrays.binarySearch(arr.asInstanceOf[Array[Int]], target)
+    }
+  }
+
+  @throws(classOf[IllegalArgumentException])
+  override def process(arr: Array[Int], size: Int = 3): Array[Int] = size match {
+    case 3 =>
+      val sorted_arr = arr.sortWith(_<_)
+      val max_len = sorted_arr.length
+      for((i_val, i) <- sorted_arr.takeWhile(_ <= 0).zipWithIndex ){
+          val maxj = sorted_arr(max_len - 1) - i_val
+          for(j_val <- sorted_arr.slice(i+1, max_len).takeWhile(_ <= maxj)){
+              val temp_sum = i_val + j_val
+              val res_idx = sorted_arr.binSearch(-temp_sum)
+              if (res_idx > -1) {
+                return Array(i_val,
+                             j_val, sorted_arr(res_idx))
+            }
+          }
+      }
+      throw new SumToZeroNotFound
+    case _ => throw new IllegalArgumentException("only support size of three for now")
+  }
+
+  private def validateSize(size: Int) = {
+    if(size > 3) throw new Exception("only accepts size of 3 for now")
+  }
+}
+
+object SumToZero{
+
+  def apply(impl: String) = impl match{
+    case "with_bin_search" => SumToZeroBinSearch
+    case _ => SumToZeroBruteForce
+  }
+}
--- jvm_langs/test/scala/org/bcajes/katas/test_sum_to_zero.scala
+++ jvm_langs/test/scala/org/bcajes/katas/test_sum_to_zero.scala
@@ -3,30 +3,36 @@ import org.scalatest.junit._
 import org.scalatest._
 import scala.runtime.ScalaRunTime.stringOf
 
+
 @RunWith(classOf[JUnitRunner])
-class TestSumToZero extends FunSuite {
+class TestSumToZero extends FunSuite{
+
+  final val methods = Array("brute_force", "with_bin_search")
+
+  def testAllMethods(arr: Array[Int], expected_res: String = "",
+                      methods: Array[String] = this.methods) = {
+    for (method <- methods){
+      println("testing: " + method)
+      val stz = SumToZero(method)
+      val res = stz.process(arr)
+      val str_res = stringOf(res.sortWith(_<_))
+      assert( str_res == expected_res,
+       "res: " + str_res)
+     }
+  }
 
   test("should return entire array that sums to zero") {
-  	val stz = new SumToZero
-	  val res = stz.process(Array(1,2,-3))
-	  val str_res = stringOf(res)
-	    assert( str_res == "Array(1, 2, -3)",
-		    "res: " + str_res)
+	  testAllMethods(Array(1,2,-3), "Array(-3, 1, 2)")
   }
 
   test("should raise NotFound exception if no combination sums to zero"){
-    val stz = new SumToZero
-    intercept[SumToZeroNotFound]{
-      stz.process(Array(1,2,3,4))
-    }
+    this.methods.foreach(method =>
+      intercept[SumToZeroNotFound]{
+        testAllMethods(Array(1,2,3), "", Array(method))
+    })
   }
-
   test("should return first set of nums that sum to zero"){
-    val stz = new SumToZero
-    val res = stz.process(Array(125, 124, -100, -25, 1, 2, -3, 10, 100))
-    val str_res = stringOf(res)
-    assert( str_res == "Array(125, -100, -25)",
-      "res: " + str_res)
+    testAllMethods(Array(125, 124, -100, -25, 1, 2, -3, 10, 100), "Array(-100, -25, 125)")
   }
 
 
